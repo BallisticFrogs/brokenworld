b77bc1db13a1f9f7454b5ae607891ea77d6fe2eb
Guillaume Tinon <guillaume.tinon@gmail.com>
2021-01-29 23:50:56 +0100

    Add player hand.

---
+++ Assets/Scripts/Layers.cs
@@ -0,0 +1,9 @@
+public class Layers
+{
+    public readonly static int ISLAND = 6;
+}
+
+public class Masks
+{
+    public readonly static int ISLAND = 1 << Layers.ISLAND;
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,22 @@
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speedFactor;
+    public float handHoverDst;
+
+    [SceneObjectsOnly] public GameObject hand;
+
+    private Camera cam;
+    private Ray handRay = new Ray();
+    private RaycastHit handRaycastHit = new RaycastHit();
+    private Vector3 handCoords = new Vector3();
+
+    private void Awake()
+    {
+        cam = Camera.main;
+    }
 
     void Start()
     {
@@ -10,12 +24,40 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        Move();
+        MoveHand();
+    }
+
+    private void Move()
+    {
         var dx = Input.GetAxis("Horizontal");
         var dz = Input.GetAxis("Vertical");
-
         if (dx != 0 || dz != 0)
         {
             transform.position += new Vector3(dx, 0, dz) * speedFactor;
         }
+    }
+
+    private void MoveHand()
+    {
+        // find if we hit an island
+        var ray = cam.ScreenPointToRay(Input.mousePosition);
+        var hasHit = Physics.Raycast(ray, out handRaycastHit, 100, Masks.ISLAND);
+        if (hasHit)
+        {
+            handCoords = handRaycastHit.point;
+            handCoords.y += handHoverDst;
+        }
+        else
+        {
+            // find intersection point with the base plane
+            Plane plane = new Plane(Vector3.up, 0);
+            if (plane.Raycast(ray, out var distance))
+            {
+                handCoords = ray.GetPoint(distance);
+            }
+        }
+
+        hand.transform.position = handCoords;
     }
 }
